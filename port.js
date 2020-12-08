9be9eb34919ba2c0c2b13e13cd5975df42a4e812
KoreZhong <75667447+KoreZhong@users.noreply.github.com>
2020-12-08 18:56:23 +0800

    Add files via upload

---
+++ port.js
@@ -0,0 +1,39 @@
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+}
+
+function myFunction() {
+  document.getElementById("myDropdown").classList.toggle("show");
+}
+
+
+// Close the dropdown menu if the user clicks outside of it
+window.onclick = function(event) {
+  if (!event.target.matches('.dropbtn1')) {
+    var dropdowns1 = document.getElementsByClassName("dropdown-content1");
+    var i;
+    for (i = 0; i < dropdowns1.length; i++) {
+      var openDropdown1 = dropdowns1[i];
+      if (openDropdown1.classList.contaians('show')) {
+        openDropdown1.classList.remove('show');
+      }
+    }
+  }
+}
+
+
+window.onclick = function(event) {
+  if (!event.target.matches('.dropbtn2')) {
+    var dropdowns2 = document.getElementsByClassName("dropdown-content2");
+    var i;
+    for (i = 0; i < dropdowns2.length; i++) {
+      var openDropdown2 = dropdowns2[i];
+      if (openDropdown2.classList.contains('show')) {
+        openDropdown2.classList.remove('show');
+      }
+    }
+  }
+}
+
+
+
